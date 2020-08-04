57f42a04dfef4dda3ab4189ed1f4778de98837c9
zhangrenxiang <1521036185@qq.com>
2020-08-04 10:56:16 +0800

    完成登录功能，保存token到sessionStorage

--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 全局样式
+import './assets/css/global.css'
+// 引入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 配置axios
+import axios from 'axios'
+axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 new Vue({
   router,
---
+++ src/views/Login.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt />
+      </div>
+      <!-- 登录表单区域 -->
+      <el-form ref="loginFormRef" label-width="0px" class="login_form" :model="loginForm" :rules='loginRules'>
+        <!-- 用户名 -->
+        <el-form-item  prop="username">
+          <el-input  prefix-icon="el-icon-user" v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item  prop="password">
+          <el-input  type="password" prefix-icon="el-icon-lock" v-model="loginForm.password"></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            // 登陆表单数据对象
+            loginForm: {
+                username: 'admin',
+                password: '123456'
+            },
+            // 表单验证规则
+            loginRules: {
+                // 验证用户名
+                username: [
+                    {required: true, message: '请输入登录名', trigger: 'blur'},
+                    {min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur'}
+                ],
+                // 验证密码
+                password: [
+                    {required: true, message: '请输入登录密码', trigger: 'blur'},
+                    {min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur'}
+                ]
+            }
+        }
+    },
+    methods: {
+        // 重置按钮，重置登录表单
+        resetLoginForm() {
+            this.$refs.loginFormRef.resetFields();
+        },
+        login(){
+            this.$refs.loginFormRef.validate(async valid => {
+                if (!valid) {
+                    return
+                }
+                const {data: res} = await this.$axios.post('login',this.loginForm)
+                if (res.meta.status !== 200) {
+                    return this.$message({
+                        message: '登录失败,用户名或密码错误！',
+                        type: 'warning'
+                    })
+                }
+                this.$message({
+                    message: '登录成功',
+                    type: 'success'
+                })
+                // 将登录成功之后的token，token只在当前网站打开期间生效，
+                // 所以保存到客户端的sessionStorage中
+                console.log(res)
+                window.sessionStorage.setItem('token', res.data.token);
+                // 跳转到后台主页 /home
+                this.$router.push('/home');
+            })
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.login_container {
+  height: 100%;
+  background: #e3edf7;
+  .login_box {
+    width: 450px;
+    height: 300px;
+    background: #fff;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    .avatar_box {
+      height: 130px;
+      width: 130px;
+      border: 1px solid #eee;
+      border-radius: 50%;
+      padding: 10px;
+      box-shadow: 0 0 10px #ddd;
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: #fff;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+      }
+    }
+  }
+}
+
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
